9479df1ae22e1112f32c4434f42451b046af5deb
TruongDu <truongdu.td88@gmailcom>
2020-05-09 18:31:43 +0700

    why choose us, service update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
+// SearCH Click
 var search1 = document.getElementById("search");
 var searchFrom = document.getElementsByClassName('search_click');
-console.log(searchFrom);
 search1.addEventListener('click',search_btn);
 function search_btn () {
 
@@ -8,4 +8,35 @@ function search_btn () {
 		var addClassSearch = searchFrom[i].classList.add('show-search');
 	}
 	
+}
+// Tab services
+var tab = document.getElementsByClassName("project__item");
+function openItem(item) {
+	if(item === "All"){
+		 for ( var i = 0; i < tab.length; i++) {
+		tab[i].style.display = "block"; 
+		}
+	} else {
+		for ( var i = 0; i < tab.length; i++) {
+		tab[i].style.display = "none";  
+		}
+		var item_tab = document.getElementsByClassName(item);
+		for ( var j=0 ; j<item_tab.length; j ++){
+			console.log(item_tab[j]);
+			item_tab[j].style.display = 'block';
+		}
+	}
+}
+
+//tab why choose us
+var tab__why = document.getElementsByClassName("content-right");
+function openWhy(item) {
+	for ( var i = 0; i < tab__why.length; i++) {
+	tab__why[i].style.display = "none";  
+	}
+	var item_tab = document.getElementsByClassName(item);
+	for ( var j=0 ; j<item_tab.length; j ++){
+		console.log(item_tab[j]);
+		item_tab[j].style.display = 'block';
+	}
 }
--- js/slide.js
+++ js/slide.js
@@ -11,12 +11,20 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-    var i;
+    // var i;
+    //   slideIndex++;
+    //   if (slideIndex > slides.length) {slideIndex = 1}    
+    //   for (i = 0; i < dots.length; i++) {
+    //     dots[i].className = dots[i].className.replace(" active", "");
+    //   }
+    //   slides[slideIndex-1].style.display = "block";  
+    //   dots[slideIndex-1].className += " active";
+    //   setTimeout(showSlides, 3000); // Change image every 2 seconds
     if(n > slides.length) {
       slideIndex = 1
     }
     if(n < 1) {
-      slideIndex = slides.length
+      slideIndex = slides.length;
     }
     for(i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
